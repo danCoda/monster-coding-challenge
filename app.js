f964d473fae6e8f9a6674c229e0b446eb04aff55
Daniel Son <dson.email@gmail.com>
2021-03-26 18:09:50 +1100

    Responsive done. % calculation not working correctly.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const numberArea = document.querySelector("#numbers");
+const scientificArea = document.querySelector("#scientificInput");
 const numberZeroRow = document.querySelector("#zeroRow");
 const operatorArea = document.querySelector("#operators");
 const extraArea = document.querySelector("#extras");
@@ -7,7 +8,8 @@ const display = document.querySelector("#display");
 const BUTTONS = {
     numbers: [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "."],
     operators: ["÷", "×", "−", "+", "="],
-    extras: ["C", "±", "%"]
+    extras: ["C", "±", "%"],
+    scientific: ["(", ")", "mc", "m+", "mr", "2nd", "x2", "x3", "xy", "ex", "10x", "(", ")", "mc", "m+", "mr", "2nd", "x2", "x3", "xy", "ex", "10x", "(", ")", "mc", "m+", "mr"]
 }
 insertButtons();
 
@@ -15,6 +17,7 @@ function insertButtons() {
     insertNumbers();
     insertOperators();
     insertExtras();
+    insertScientific();
 
     function insertNumbers() {
         BUTTONS.numbers.forEach(b => {
@@ -47,6 +50,14 @@ function insertButtons() {
             `);
         });
     }
+
+    function insertScientific() {
+        BUTTONS.scientific.forEach(b => {
+            scientificArea.insertAdjacentHTML("beforeend", `
+                <div class="button extra scientific">${b}</div>
+            `);
+        });
+    }
 }
 
 let inputRecord = [];
@@ -69,7 +80,7 @@ function processInput(key) {
                 console.log("Currently: ", currentDisplay);
 
                 if (currentDisplay) {
-                    currentInputs[0] = currentDisplay;    
+                    currentInputs[0] = currentDisplay;
                     currentInputs[1] = key;
                 } else if (currentInputs[1]) {
                     currentInputs[1] = key;
@@ -78,31 +89,82 @@ function processInput(key) {
         }
 
         if (key === ".") {
-            if (currentInputs[2]) {
-                addDigitAtIndex(2, key);
-            } else if (currentInputs[1]) {
-                addDigitAtIndex(1, key);
+            if (currentInputs[1]) {
+                // An operator is selected. Deal with second number. 
+                addDecimalPointInIndex(2);
+                updateDisplayWithIndex(2);
+
             } else {
-                // index 0 is empty; add 0 and ".".
-                addDigitAtIndex(0, "0");
-                addDigitAtIndex(0, key);
+                // Deal with first number. 
+                addDecimalPointInIndex(0);
+                updateDisplayWithIndex(0);
+
+            }
+
+            function addDecimalPointInIndex(i) {
+                if (currentInputs[i]) {
+                    if (!currentInputs[i].includes(".")) addDigitAtIndex(i, key);
+                } else {
+                    addDigitAtIndex(i, 0);
+                    addDigitAtIndex(i, key);
+                }
             }
         }
 
         if (key === "=") {
             if (!currentInputs.length) return;
+
             if (currentInputs[2]) {
-                updateDisplay(getCurrentTotal());
+                try {
+                    updateDisplay(getCurrentTotal());
+                } catch (e) {
+                    display.innerHTML = e;
+                }
             } else {
                 updateDisplayWithIndex(0);
-
             }
+            //currentInputs[2] ? updateDisplay(getCurrentTotal()) : updateDisplayWithIndex(0);
             resetCalculatorMemory();
         }
 
         if (key === "C") {
             resetCalculatorMemory();
             updateDisplay();
+        }
+
+        if (key === "±") {
+            if (currentInputs[1]) {
+                // An operator is selected. Deal with second number. 
+                makePositveOrNegativeAtIndex(2);
+                updateDisplayWithIndex(2);
+            } else {
+                // Deal with first number. 
+                makePositveOrNegativeAtIndex(0);
+                updateDisplayWithIndex(0);
+            }
+
+            function makePositveOrNegativeAtIndex(i) {
+                if (currentInputs[i]) {
+                    if (!currentInputs[i].includes("-")) {
+                        currentInputs[i] = "-" + currentInputs[i];
+                    } else {
+                        currentInputs[i] = currentInputs[i].replace("-", "");
+                    }
+                } else {
+                    currentInputs[i] = "-";
+                }
+            }
+        }
+
+        if (key === "%") {
+            if (currentInputs[1]) {
+                addDigitAtIndex(2, key);
+                updateDisplayWithIndex(2);
+            } else {
+
+                addDigitAtIndex(0, key);
+                updateDisplayWithIndex(0);
+            }
         }
 
     } else { // Is a number.
@@ -124,9 +186,17 @@ function resetCalculatorMemory() {
 }
 
 function getCurrentTotal() {
-    let num1 = +currentInputs[0];
-    let num2 = +currentInputs[2];
-    let total;
+    let num1, num2, total;
+
+    try {
+        [num1, num2] = getValues();
+        [num1, num2].forEach(n => {
+            // Todo: Work out the percentages.
+        })
+    } catch (e) {
+        throw e;
+    }
+
 
     switch (currentInputs[1]) {
         case "÷":
@@ -142,28 +212,46 @@ function getCurrentTotal() {
             total = num1 + num2;
             break;
     }
+
     console.log(total, num1, num2, currentInputs);
     return total;
-}
 
-function addDigitAtIndex(i, key) {
-    if (!currentInputs[i]) {
-        currentInputs[i] = key;
-    } else {
-        currentInputs[i] += key;
+    function getValues() {
+        let valueIndexes = [0, 2];
+        let num1, num2;
+        valueIndexes.forEach(i => {
+            // If the input starts with "%" or "-%", is invalid. 
+            if (currentInputs[i].charAt(0) === "%" || currentInputs[i].charAt(0) === "-" && currentInputs[i].charAt(1) === "%") {
+                throw "Error in the input";
+            } else {
+                if (i === 0) {
+                    num1 = currentInputs[i];
+                } else {
+                    num2 = currentInputs[i];
+                }
+            }
+        });
+
+        return [num1, num2];
     }
 }
 
-function updateDisplayWithIndex(i) {
-    display.innerHTML = currentInputs[i].toLocaleString();
-    display.setAttribute("data-value", currentInputs[i]);
-
+function addDigitAtIndex(i, key) {
+    if (!currentInputs[i]) currentInputs[i] = "";
+    currentInputs[i] += key;
 }
 
-function updateDisplay(value) {
-    if(value) {
-        display.innerHTML = value.toLocaleString();
-        display.setAttribute("data-value", value.toLocaleString());
+function updateDisplayWithIndex(i) {
+    console.log("hello: ", currentInputs[i].toLocaleString());
+    display.innerHTML = currentInputs[i].toLocaleString();
+    display.setAttribute("data-value", currentInputs[i]);
+}
+
+function updateDisplay(number) {
+    console.log("Value: ", number);
+    if (!isNaN(number)) { // If it's a number, including 0:
+        display.innerHTML = number.toLocaleString();
+        display.setAttribute("data-value", number.toLocaleString());
     } else {
         display.innerHTML = "";
         display.removeAttribute("data-value");
